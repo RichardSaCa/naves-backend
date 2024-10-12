0fafc0da2b0aab1129a028e9fe37be487eee1eaf
Richard Sanchez <caicedorich@unicauca.edu.co>
2024-10-12 10:24:55 -0500

    firs commit

---
+++ src/test/java/com/estacion/naves/NavesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacion.naves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
